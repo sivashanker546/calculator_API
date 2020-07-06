addbfbdb81f065d0cabcb81331a6ca52ff30846a
sivashanker <dreamsivashanker@gmail.com>
2020-07-07 00:42:32 +0530

    worked on handling error exceptions

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -13,6 +13,7 @@ trait ApiResponser
     public function successResponse($data, $code=Response::HTTP_OK)
     {
         return response()->json(['data'=>$data],$code);
+        //return response($data,$code)->header('Content-Type', 'application/json');
     }
     /**
      * BUILD ERROR RESPONSE
@@ -23,5 +24,15 @@ trait ApiResponser
     public function errorResponse($message, $code)
     {
         return response()->json(['error'=>$message,'code'=>$code],$code);
+    }
+    /**
+     * BUILD ERROR RESPONSE
+     * @param string|array $message
+     * @param int $code
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function errorMessage($message, $code)
+    {
+        return response($message,$code)->header('Content-Type', 'application/json');
     }
 }
